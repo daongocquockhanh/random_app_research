22f1d1a856c18380b2cf853f30347ab6550e7867
APAC\DKA3HC <Khanh.DaoNgocQuoc@vn.bosch.com>
2023-11-16 15:22:29 +0700

    Update set up POM

---
+++ random-app/src/main/java/com/bosch/random_app/MainApp.java
@@ -0,0 +1,16 @@
+package com.bosch.random_app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
